9abfae7f60527b1095aaad788f3ab77fa13943cc
Zed Chang <KinKuoChang@dingtalk.com>
2018-11-16 14:27:21 +0800

    It is work!

---
+++ index.js
@@ -0,0 +1,31 @@
+const Event = require('events');
+
+class Mutex extends Event {
+  constructor(){
+      super();
+      this._events = [];
+  }
+  async lock(){
+    const event = this._events[0];
+    this._events.push(Symbol());
+    if(event == undefined){
+      return;
+    }else{
+      return new Promise(
+        resolve =>{
+          this.on(event, resolve);
+        }
+      );
+    }
+  }
+  unlock(){
+    let event = this._events.shift();
+    console.log(event);
+    if(event !== undefined ){
+      this.emit(event);
+    }
+  }
+}
+module.exports={
+  Mutex,
+};
